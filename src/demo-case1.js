6c11a1f374b4192b3680c7ef4e540d69d3d23bfc
qiushaowei <qiushaowei@360.net>
2018-06-28 15:24:13 +0800

    up

--- src/demo-case1.js
+++ src/demo-case1.js
@@ -1,5 +1,17 @@
 
-import JSPDD from './jspdd.js';
+import Example from './example.js';
 
-console.log( 111 );
-console.log( JSPDD );
+let demo = new Example( [ 
+    { 
+        url: "./data/case1/srcdata.json"
+        , type: 'srcdata'
+    }
+    , { 
+        url: "./data/case1/newdata.json"
+        , type: 'newdata'
+    }
+    , { 
+        url: "./data/case1/descdata.json"
+        , type: 'descdata'
+    }
+]);
---
+++ src/example.js
@@ -0,0 +1,33 @@
+
+import 'whatwg-fetch';
+import JSPDD from './jspdd.js';
+
+
+export default class Example {
+    constructor( api ) {
+        this.DATA = {};
+        this.api = api;
+
+        this.run();
+    }
+    run(){
+        if( !this.api.length ) {
+            this.proc();
+            return;
+        }
+        let item = this.api.shift();
+
+        fetch( item.url, { rnd: Date.now() } )
+        .then(function(response) {
+            return response.json()
+        }).then( ( json ) => {
+            this.DATA[ item.type ] = json;
+            this.run();
+        })
+    }
+
+    proc() {
+        console.log( 'api data:', this.DATA );
+    }
+}
+
